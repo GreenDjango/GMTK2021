546174ad0315e0d60fcefcaa99e89a18e86dbc91
Théo <52924057+GreenDjango@users.noreply.github.com>
2021-06-13 12:01:57 +0200

    FIX: Player offset

--- src/scripts/Game.gd
+++ src/scripts/Game.gd
@@ -13,10 +13,11 @@ func _process(_delta : float):
 		$CanvasLayer/DebugLabel.text = str(fps) + "fps " + str(process_time).pad_decimals(2) + "ms"
 
 func _input(event : InputEvent):
-	if event.is_pressed():
+	if event.is_action_pressed("ui_accept"):
 		spawn_mob()
 
 func spawn_mob():
 	var scene : Node2D = mob_scene.instance()
-	scene.position = Vector2(180, 116)
+	rand_range(0, 10)
+	scene.position = Vector2(160 + rand_range(0, 20), 100 + rand_range(0, 10))
 	$YSort.add_child(scene)
